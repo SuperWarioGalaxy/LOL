fe968d6d4415cd37cd9af3a638dda52e65d4176b
SuperWarioGalaxy <marpermarr@gmail.com>
2024-10-14 15:33:10 +0200

    cambios finales

--- src/js/index.js
+++ src/js/index.js
@@ -31,36 +31,24 @@ function pushChampion(champion) {
 
 const showChampions = async () => {
     const r_champ = document.getElementById("rchamp");
-    const modal = document.getElementById("modal");
+    
     champions.forEach((champ) => {
         const img = `https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/${champ.image.full}`;
 
-        modal.innerHTML +=`
-        <div class="detail">
-            ${champ.blurb}
-            <span class="close">&times;</span>
-        </div>
-        `
         r_champ.innerHTML +=    `<div class="card" >
-                                    <button class="open">${champ.name}</button>
+                                    <h3>${champ.name}</h3>
                                     <hr>
-                                    ${champ.title}<br>
+                                    <p class="title">${champ.title}</p><br>
                                     <img class="front" src="${img}"><br>
                                     <div class="types">
                                         ${champ.tags.map(tag=>`${tag}`).join(' | ')}
+                                    </div>
+                                    <div class="text-container">
+                                        <p>${champ.blurb}</p>
                                     </div>
                                 </div>`
     })
 }
 
-const open = document.querySelector(".open");
-open.addEventListener("click", function() {
-    
-    document.querySelector('.modal').style.display = "block";
-});
-const close = document.querySelector(".close");
-close.addEventListener("click", function() {
-    document.querySelector('.modal').style.display = "none";
-});
 
 // const img = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ.id + '_0.jpg'}`
